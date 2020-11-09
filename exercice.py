879ba0ee04689fdefc99762e9b15604ea74db7f1
Clarens-M <clarens.mpouo@polymtl.ca>
2020-11-09 23:44:47 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -8,7 +8,6 @@ import numpy as np
 
 # TODO: Définissez vos fonctions ici (il en manque quelques unes)
 def linear_values() -> np.ndarray:
-    a = np.arange(-1.3, 2.5, 0.01875)
     return a
 
 def coordinate_conversion(cartesian_coordinates: np.ndarray) -> np.ndarray:
