650aaad2e73f61f4ea0693ca5e15d5c20734320a
Clarens-M <70984346+Clarens-M@users.noreply.github.com>
2020-11-09 19:50:24 -0500

    Update exercice.py

--- exercice.py
+++ exercice.py
@@ -4,10 +4,12 @@
 
 # TODO: Importez vos modules ici
 import numpy as np
+import matplotlib.pyplot as plt
+
 
 # TODO: Définissez vos fonctions ici (il en manque quelques unes)
 def linear_values() -> np.ndarray:
-    return np.linspace(-1.3, 2.5, 64)
+    return np.linspace(-1.3, 2.5, num=64)
 
 def coordinate_conversion(cartesian_coordinates: np.ndarray) -> np.ndarray:
     return np.array([(np.sqrt(co[0]**2 + co[1]**2), np.arctan(co[1], co[0])) for co in cartesian_coordinates])
@@ -17,14 +19,20 @@ def find_closest_index(values: np.ndarray, number: float) -> int:
     return np.abs(values-number).argmin()
 
 def exo4():
-    valeurs = np.linspace(-1, 1, 250)
-    a = np.array([x, x**2 * np.sin(1/(x**2)) + x] for x in valeurs)
-    return a
+    x = np.linspace(-1, 1, num=250)
+    y = x**2 * np.sin(1/(x**2)) + x
+
+    plt.scatter(x, y)
+    plt.show()
+
+    return
 
 if __name__ == '__main__':
     # TODO: Appelez vos fonctions ici
-    print(linear_values())
+#    print(linear_values())
 #    print(coordinate_conversion(np.array([])))
+
+    exo4()
 
 
     pass
